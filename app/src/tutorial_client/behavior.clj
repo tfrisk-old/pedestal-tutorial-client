dea047357fc392dc8382cf84d2730709a411788a
Teemu Frisk <teemu.frisk@gmail.com>
2013-10-30 08:48:38 +0200

    Add derived total-count

--- app/src/tutorial_client/behavior.clj
+++ app/src/tutorial_client/behavior.clj
@@ -9,6 +9,10 @@
 (defn swap-transform [_ message]
   (:value message))
 
+;derive function
+(defn total-count [_ nums]
+  (apply + nums))
+
 ;effect function
 (defn publish-counter [count]
   [{msg/type :swap msg/topic [:other-counters] :value count}])
@@ -23,7 +27,13 @@
      [:inc  [:my-counter] inc-transform]
      [:swap [:**]         swap-transform]]
    :effect #{[#{[:my-counter]} publish-counter :single-val]}
+   :derive #{[
+     #{[:my-counter] [:other-counters :*]} ;inputs
+     [:total-count] ;output
+     total-count ;derive function
+     :vals]} ;input specifier
    :emit [
      {:init init-main}
-     [#{[:my-counter] [:other-counters :*]} (app/default-emitter [:main])]]})
+     [#{[:my-counter] [:other-counters :*] [:total-count]}
+       (app/default-emitter [:main])]]})
 
